ebee590ddb796437dad4c1cd3e225b0214f49453
pyrots <42144426+pyrots@users.noreply.github.com>
2026-05-30 10:25:43 +0200

    Initialize solargenflow component with setup functions

---
+++ custom_components/solargenflow/__init__.py
@@ -0,0 +1,12 @@
+from .const import DOMAIN
+
+PLATFORMS = ["sensor"]
+
+
+async def async_setup_entry(hass, entry):
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass, entry):
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
